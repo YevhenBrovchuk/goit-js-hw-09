7fcfe2ee915439454a11f04fd4c442c888efba93
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-07-10 14:52:38 +0300

    comit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+const btnStartEl=document.querySelector("button[data-start]")
+const btnStopEl = document.querySelector("button[data-stop]")
+const bodyEl=document.querySelector("body")
+// console.dir(btnStartEl.textContent);
+// console.dir(btnStopEl.textContent);
+// bodyEl.style.backgroundColor = "teal";
 
+
+btnStartEl.addEventListener("click",  handlerBodyColor)
+
+
+function handlerBodyColor(evt)
+{
+    evt.target.disabled=true
+    console.log(evt.target);
+}
