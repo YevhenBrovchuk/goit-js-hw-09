c6baac19ce9242f1930dd3f536cea55bbd111d56
YevhenBrovchuk <124561578+YevhenBrovchuk@users.noreply.github.com>
2023-07-10 21:40:17 +0000

    Deploying to gh-pages from @ YevhenBrovchuk/goit-js-hw-09@168ea0c6d60e4ca283e4219dd2c6a02abdf663ca 🚀

--- 01-color-switcher.597da5c5.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]"),document.querySelector("body");t.addEventListener("click",(function(t){t.target.disabled=!0,console.log(t.target)}))}();
-//# sourceMappingURL=01-color-switcher.597da5c5.js.map
---
+++ 01-color-switcher.ebf97ec8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(t){a(t.target,e),d=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(e){a(e.target,t),clearInterval(d)})),window.addEventListener("load",(function(){e.disabled=!0}));let d=null;function a(t,e){!1===t.disabled&&(t.disabled=!0,e.disabled=!1)}
+//# sourceMappingURL=01-color-switcher.ebf97ec8.js.map
