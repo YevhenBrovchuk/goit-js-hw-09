f15be4f9bf4a4745c1ed4bd103a0e05da5a7f1f6
YevhenBrovchuk <124561578+YevhenBrovchuk@users.noreply.github.com>
2023-07-10 11:53:11 +0000

    Deploying to gh-pages from @ YevhenBrovchuk/goit-js-hw-09@7fcfe2ee915439454a11f04fd4c442c888efba93 🚀

---
+++ 01-color-switcher.40fad5ca.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]"),document.querySelector("body");t.addEventListener("click",(function(t){t.target.disabled=!0,console.log(t.target)}));
+//# sourceMappingURL=01-color-switcher.40fad5ca.js.map
---
+++ 01-color-switcher.597da5c5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]"),document.querySelector("body");t.addEventListener("click",(function(t){t.target.disabled=!0,console.log(t.target)}))}();
+//# sourceMappingURL=01-color-switcher.597da5c5.js.map
